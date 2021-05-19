6e7483b269bd57a110503d8a97f7934392f1bf42
17143 <17143@burnside.school.nz>
2021-05-19 13:53:15 +1200

    added config, starting on adding routes to main

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from flask import Flask
 
+from config import Config
+
 app = Flask(__name__)
+app.config.from_object(Config)
 
 
 @app.route('/')
 def home():
     return"hello world"
 
+@app.route("/help")
+def help():
+    return "Help desk."
 
-app.run(debug=True)
+@app.route("/Project/<string:project_name>")
+def Project(project_name):
+    return "Here is a {} Project".format(project_name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
