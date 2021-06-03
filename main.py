5d85841ef5c1b7eef7726d1daf9d75474b4a45af
17143 <17143@burnside.school.nz>
2021-06-03 12:03:11 +1200

    starting to add templates, setup towards adding SQLAlchemy aswell as adding routes to main.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import sqlite3
 from config import Config
 
@@ -14,16 +14,20 @@ def home():
 def help():
     return "Help desk."
 
+@app.route("/projects")
+def models():
+    return "Projects"
+
+
 @app.route("/Project/<string:project_name>")
 def Project(project_name):
     connection = sqlite3.connect('portfolio.db')
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM Project where name = ? ", (project_name))
+    cursor.execute("SELECT * FROM Project where name = ? ", (project_name,))
     project = cursor.fetchone()
     connection.close()
-    return "<h1>{}</h1><p>{}</p>".format(project[1], project[6])
+    return "<h1>{}</h1><p>{}</p>".format(project[1], project[3])
 
     
-
 if __name__ == "__main__":
     app.run(debug=True)
