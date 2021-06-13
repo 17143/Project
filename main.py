7638ba7c815484ce9f98b3fb56df8db9dab67bb6
17143 <17143@burnside.school.nz>
2021-06-14 09:26:46 +1200

    added first route to all projects, fixed some issues regarding config, changed heading colour to midnight purple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from os import name
 from flask import Flask, render_template, request, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, model
 import sqlite3
 from config import Config
 
@@ -21,17 +22,21 @@ def help():
 @app.route("/projects")
 def projects():
     projects = models.Project.query.all()
-    return render_template('projects.html')
+    return render_template('projects.html', projects=projects)
 
+@app.route('/project/<int:id>')
+def project(id):
+  project = models.Pizza.query.filter_by(id=id).first_or_404()
+  return render_template('pizza.html', project=project)
 
-@app.route("/Project/<string:project_name>")
-def Project(project_name):
-    connection = sqlite3.connect('portfolio.db')
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM Project where name = ? ", (project_name,))
-    project = cursor.fetchone()
-    connection.close()
-    return "<h1>{}</h1><p>{}</p>".format(project[1], project[3])
+# @app.route("/Project/<string:project_name>")
+# def Project(project_name):
+#     connection = sqlite3.connect('portfolio.db')
+#     cursor = connection.cursor()
+#     cursor.execute("SELECT * FROM Project where name = ? ", (project_name,))
+#     project = cursor.fetchone()
+#     connection.close()
+#     return "<h1>{}</h1><p>{}</p>".format(project[1], project[3])
 
     
 if __name__ == "__main__":
